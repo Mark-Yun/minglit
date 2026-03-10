aab4d7f4a618e2aa7bca6a2455497e83540ae7dc
Mark Yun <only.hyeok@gmail.com>
2026-03-10 21:21:05 +0900

    feat(bug-report): add layout dump capture to bug reports

--- shared/packages/minglit_kit/lib/minglit_ui.dart
+++ shared/packages/minglit_kit/lib/minglit_ui.dart
@@ -35,4 +35,5 @@ export 'src/ui/widgets/party/event_card.dart';
 export 'src/ui/widgets/party/location_map_view.dart';
 export 'src/utils/environment_info.dart';
 export 'src/utils/feedback_ext.dart';
+export 'src/utils/layout_dump.dart';
 export 'src/utils/splash_screen.dart';
