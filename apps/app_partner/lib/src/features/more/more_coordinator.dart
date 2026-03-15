599d9c831aa2c4f9865f9479091c56766697a6bb
Mark Yun <only.hyeok@gmail.com>
2026-03-15 19:22:48 +0900

    feat(app_partner): add NotificationSettingsRoute and MoreCoordinator

---
+++ apps/app_partner/lib/src/features/more/more_coordinator.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:app_partner/src/routing/app_router.dart';
+import 'package:app_partner/src/routing/app_routes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final moreCoordinatorProvider = Provider<MoreCoordinator>((ref) {
+  return MoreCoordinator(ref.read(goRouterProvider));
+});
+
+class MoreCoordinator {
+  MoreCoordinator(this._router);
+
+  final GoRouter _router;
+
+  void pushNotificationSettings() {
+    unawaited(_router.push(const NotificationSettingsRoute().location));
+  }
+
+  void pushMemberList(String partnerId) {
+    unawaited(_router.push(MemberListRoute(partnerId: partnerId).location));
+  }
+
+  void pushVerificationManage() {
+    unawaited(_router.push(const VerificationManageRoute().location));
+  }
+}
